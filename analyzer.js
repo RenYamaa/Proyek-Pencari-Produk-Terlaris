cceaba71aa28ca673605b1f075de099716bb5f08
Michael <michaelgrowt24@gmail.com>
2025-10-04 01:14:08 +0700

    feat: Selesaikan Logic #1 dan #2 (MAX/MIN), tambahkan .gitignore dan dotenv setup.

---
+++ analyzer.js
@@ -0,0 +1,54 @@
+const { json } = require('stream/consumers');
+const conn = require('./koneksi.js');
+require('dotenv').config(); // <------ Untuk membaca .env
+
+const q = "SELECT * From produk";
+conn.query(q, (err, results) => {
+    if(err){
+        console.log(err);
+        return conn.end('Database error');
+    }
+
+    const hargaArray = results.map(price => price.Harga_Satuan);
+    //Mencari max dan min value dari array harga price satuan pada database
+    const maxValue = Math.max(...hargaArray);
+    const minValue = Math.min(...hargaArray);
+    console.log("Harga Tertinggi nya adalah: " + Math.max(...hargaArray));
+    console.log("Harga Terendah nya adalah: " + Math.min(...hargaArray));
+
+    const fullObjectMax = results.find(product => product.Harga_Satuan == maxValue);
+    const fullObjectMin = results.find(product => product.Harga_Satuan == minValue);
+    
+    console.log("Full OBJ Max: ", fullObjectMax);
+    console.log("Full OBJ Min: ", fullObjectMin);
+});
+
+const q2 = `
+    SELECT * FROM produk AS P 
+    JOIN detail_penjualan AS D 
+    ON P.ID_Produk = D.ID_Produk
+`;
+
+conn.query(q2, (err, results) => {
+    if(err){
+        console.log(err);
+        return conn.end('Database error');
+    }
+
+    const resultArray = results.map(row => {
+        // Kalau mau selective, kyk gini
+        // nanti yang keluar ya yang dipilih aja
+        return {
+            idProduk: row.ID_Produk,
+            namaProduk: row.Nama,
+            harga: row.Harga_Satuan,
+            jumlahTerjualnya: row.Kuantitas_Terjual
+        };
+        // Tapi untuk menampilkan semua baris pakai ini, karena row isinya gabungan dari semuanya.
+        // return row;
+    });
+
+    console.log(resultArray);
+});
+
+//lanjut besok, dimana akan ada "order by" dan lain2. 
---
+++ koneksi.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+require('dotenv').config(); // <------ Untuk membaca .env
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAMA
+});
+
+conn.connect(err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to MySQL!");
+    }
+});
+
+module.exports = conn;
