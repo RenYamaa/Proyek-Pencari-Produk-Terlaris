ac2def46c843b2090ae0c712526dd67d11a7da2c
Michael <michaelgrowt24@gmail.com>
2025-10-05 22:37:21 +0700

    Final Results

--- koneksi.js
+++ koneksi.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-require('dotenv').config(); // <------ Untuk read .env
+require('dotenv').config(); // <------ Untuk membaca .env
 
 const conn = mysql.createConnection({
     host: process.env.DB_HOST,
